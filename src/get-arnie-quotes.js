ef1c20774127f20cf00db1ac805b2d1bf235d3e2
Kitty Yuan <kitty.yuan525@gmail.com>
2026-09-02 16:00:27 +1000

    feat: implement getArnieQuotes with parallel httpGet calls

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,11 +1,31 @@
 const { httpGet } = require('./mock-http-interface');
 
+const SUCCESS_KEY = 'Arnie Quote';
+const FAILURE_KEY = 'FAILURE';
+const HTTP_STATUS_OK = 200;
+
+/**
+ * @param {HttpGetResponse} response
+ * @returns {ArnieQuoteResult}
+ */
+const mapResponseToResult = ({ status, body }) => {
+  const { message } = JSON.parse(body);
+
+  if (status === HTTP_STATUS_OK) {
+    return { [SUCCESS_KEY]: message };
+  }
+
+  return { [FAILURE_KEY]: message };
+};
+
 /**
  * @param {GetArnieQuotesInput} urls
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  return Promise.all(
+    urls.map(async (url) => mapResponseToResult(await httpGet(url))),
+  );
 };
 
 module.exports = {
